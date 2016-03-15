1c6a72d7632d8ff886fbaa2464091a829c5ed06f
shazin-github <shazin.kh043@gmail.com>
2016-03-15 14:22:16 +0500

    User Resgistration updated

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -82,7 +82,20 @@ class userController extends Controller{
 		try{
         	$resp  = $this->guzzle->request('POST', $this->apiUrl.'user', ['form_params'=>$data]);
         } catch(\Exception $e){
-       		return Response::json(['success'=>false, 'msg'=>$this->makeError("Can't send request")]);
+            $response = $e->getResponse();
+
+            $responseBodyAsString = $response->getBody()->getContents();
+
+            $result = json_decode($responseBodyAsString);
+
+            if($result->status_code == 400){
+
+                return Response::json(['success'=>false, 'msg'=>$this->makeError('Email alreay exists') ]);
+
+            }else {
+
+                return Response::json(['success' => false, 'msg' => $this->makeError("Can't send request")]);
+            }
        	}
         $result = json_decode($resp->getBody());
 
